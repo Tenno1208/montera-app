da60fa0da203fdf1cc57bc4f77eb8974f8eae517
Avvatar777 <154893789+Tenno1208@users.noreply.github.com>
2026-07-30 11:36:13 +0700

    edit index php

--- api/index.php
+++ api/index.php
@@ -1,28 +1,3 @@
 <?php
 
-require __DIR__.'/../vendor/autoload.php';
-
-$app = require_once __DIR__.'/../bootstrap/app.php';
-
-echo "<pre>";
-
-try{
-
-    echo "VIEW : ";
-    var_dump($app->make('view'));
-
-}catch(Throwable $e){
-
-    echo "<h2>ERROR NIH BOS</h2>";
-
-    echo $e->getMessage();
-
-    echo "<br><br>";
-
-    echo $e->getFile();
-
-    echo "<br><br>";
-
-    echo $e->getLine();
-
-}
+require __DIR__.'/../public/index.php';
