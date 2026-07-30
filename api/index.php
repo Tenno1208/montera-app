64abbb623526bd84c3b91bc163799f382fa5fd67
Avvatar777 <154893789+Tenno1208@users.noreply.github.com>
2026-07-30 10:40:44 +0700

    edit index php

--- api/index.php
+++ api/index.php
@@ -1,2 +1,27 @@
 <?php
-require __DIR__ . '/../public/index.php';
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+try {
+
+    require __DIR__ . '/../public/index.php';
+
+} catch (Throwable $e) {
+
+    echo "<h1>ERROR NIH BOS</h1>";
+
+    echo "<pre>";
+    echo "Pesan Error : ".$e->getMessage();
+    echo "\n\n";
+
+    echo "File : ".$e->getFile();
+    echo "\n\n";
+
+    echo "Baris : ".$e->getLine();
+    echo "\n\n";
+
+    echo $e->getTraceAsString();
+    echo "</pre>";
+
+}
